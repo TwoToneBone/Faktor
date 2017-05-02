931b83fea80ed4b32a521d4fca121075ea414f70
Erik <erik.ra@gmail.com>
2017-05-02 23:06:55 +0200

    Add project files.

---
+++ Faktor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split();
+            int articles = int.Parse(inputs[0]);
+            
+            Console.WriteLine((int.Parse(inputs[1]) * articles) - (articles -1));            
+        }
+    }
+}
